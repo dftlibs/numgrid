cda3f19f40d4c4b8d3b97a7921816762b7199f02
Radovan Bast <bast@users.noreply.github.com>
2015-08-01 14:17:19 +0200

    update fortran module

--- cmake/downloaded/autocmake_fc_optional.cmake
+++ cmake/downloaded/autocmake_fc_optional.cmake
@@ -12,7 +12,7 @@
 #
 # Variables defined::
 #
-#   CMAKE_Fortran_MODULE_DIRECTORY (${PROJECT_BINARY_DIR}/include/fortran)
+#   CMAKE_Fortran_MODULE_DIRECTORY
 #
 # Variables modified::
 #
@@ -40,7 +40,8 @@ option(ENABLE_FC_SUPPORT "Enable Fortran language support" ON)
 if(ENABLE_FC_SUPPORT)
     enable_language(Fortran)
 
-    set(CMAKE_Fortran_MODULE_DIRECTORY ${PROJECT_BINARY_DIR}/include/fortran)
+    set(CMAKE_Fortran_MODULE_DIRECTORY ${PROJECT_BINARY_DIR}/modules)
+    include_directories(${PROJECT_BINARY_DIR}/modules)
 
     if(NOT DEFINED CMAKE_Fortran_COMPILER_ID)
         message(FATAL_ERROR "CMAKE_Fortran_COMPILER_ID variable is not defined!")
