5e3e675fa2ca23f0dca02291201176699affc899
Radovan Bast <bast@users.noreply.github.com>
2017-02-02 20:40:08 +0100

    avoid copying python api; fixes #6

--- api/numgrid.py
+++ api/numgrid.py
@@ -1,33 +1,47 @@
-"""
-Python handle to numgrid library.
-"""
-
-import os
 import sys
-import subprocess
+import os
+from subprocess import Popen, PIPE
 from cffi import FFI
 
-BUILD_DIR = os.path.abspath(os.path.dirname(__file__))
 
-ffi = FFI()
+def _get_env(v):
+    _v = os.getenv(v)
+    if _v is None:
+        sys.stderr.write('Error: Environment variable {0} is undefined\n'.format(v))
+        sys.exit(1)
+    return _v
 
-ffi.cdef(
-    subprocess.Popen(
-        [
-            'cc',
-            '-E',
-            '-DNUMGRID_API=',
-            '-DNUMGRID_NOINCLUDE',
-            os.path.join(BUILD_DIR, 'include', 'numgrid.h')
-        ],
-        stdout=subprocess.PIPE).communicate()[0].decode('utf-8'))
 
-if sys.platform == "darwin":
-    suffix = 'dylib'
-else:
-    suffix = 'so'
+def _get_library_suffix():
+    if sys.platform == "darwin":
+        return 'dylib'
+    else:
+        return 'so'
 
-_lib = ffi.dlopen(os.path.join(BUILD_DIR, 'lib', 'libnumgrid.%s' % suffix))
+
+def _get_lib_handle(definitions, header, library, build_dir, include_dir):
+    ffi = FFI()
+
+    interface = Popen(['cc', '-E'] + definitions + [os.path.join(include_dir, header)],
+                      stdout=PIPE).communicate()[0].decode("utf-8")
+    ffi.cdef(interface)
+
+    suffix = _get_library_suffix()
+    lib = ffi.dlopen(os.path.join(build_dir, 'lib', 'lib{0}.{1}'.format(library, suffix)))
+    return lib
+
+
+_build_dir = _get_env('NUMGRID_BUILD_DIR')
+_include_dir = os.path.dirname(os.path.realpath(__file__))
+
+_lib = _get_lib_handle(
+    ['-DNUMGRID_API=', '-DNUMGRID_NOINCLUDE'],
+    'numgrid.h',
+    'numgrid',
+    _build_dir,
+    _include_dir
+)
+
 
 # outward facing API
 new_context = _lib.new_context
@@ -36,5 +50,7 @@ generate_grid = _lib.generate_grid
 get_num_points = _lib.get_num_points
 get_grid = _lib.get_grid
 
+
 def get_version():
+    ffi = FFI()
     return ffi.string(_lib.get_version()).decode('utf-8')
