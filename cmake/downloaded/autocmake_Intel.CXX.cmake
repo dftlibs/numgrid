af8eeac286e5e0e0fe1138f6b5a729024737592c
Radovan Bast <bast@users.noreply.github.com>
2017-06-11 15:03:31 +0200

    update autocmake infrastructure

--- cmake/downloaded/autocmake_Intel.CXX.cmake
+++ cmake/downloaded/autocmake_Intel.CXX.cmake
@@ -1,6 +1,6 @@
 if(NOT DEFINED ENV{CXXFLAGS})
     if(CMAKE_CXX_COMPILER_ID MATCHES Intel)
-        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
         set(CMAKE_CXX_FLAGS_RELEASE "-O3")
         set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g")
     endif()
