98bcd04f1fc3a3532de579c2ec2756dffe242015
Radovan Bast <bast@users.noreply.github.com>
2017-10-07 21:59:03 +0200

    update fortran interface and test

--- test/test.f90
+++ test/test.f90
@@ -1,5 +1,8 @@
 program test
 
+   ! in this test we compute the grid for O
+   ! in the presence of two H in the H2O molecule
+
    use numgrid
    use, intrinsic :: iso_c_binding, only: c_ptr
 
@@ -9,22 +12,24 @@ program test
    integer              :: min_num_angular_points
    integer              :: max_num_angular_points
    integer              :: num_centers
-   real(8), allocatable :: center_coordinates(:)
-   integer, allocatable :: center_elements(:)
-   integer              :: num_outer_centers
-   real(8), allocatable :: outer_center_coordinates(:)
-   integer, allocatable :: outer_center_elements(:)
-   integer              :: num_shells
-   integer, allocatable :: shell_centers(:)
-   integer, allocatable :: shell_l_quantum_numbers(:)
-   integer, allocatable :: shell_num_primitives(:)
-   real(8), allocatable :: primitive_exponents(:)
+   integer              :: proton_charges(3)
+   real(8)              :: x_coordinates_au(3);
+   real(8)              :: y_coordinates_au(3);
+   real(8)              :: z_coordinates_au(3);
+   real(8)              :: alpha_max;
+   integer              :: max_l_quantum_number;
+   real(8)              :: alpha_min(3)
    integer              :: num_points
+   real(8), allocatable :: grid_x_au(:)
+   real(8), allocatable :: grid_y_au(:)
+   real(8), allocatable :: grid_z_au(:)
+   real(8), allocatable :: grid_w(:)
+   integer              :: center_index
    integer, parameter   :: io_unit = 13
-   real(8)              :: ref(4)
-   integer              :: i, j, k
+   real(8)              :: ref(4), own(4)
+   integer              :: ipoint
+   integer              :: j
    real(8)              :: error
-   real(8), pointer     :: grid(:)
    type(c_ptr)          :: context
 
    radial_precision = 1.0d-12
@@ -33,157 +38,78 @@ program test
 
    num_centers = 3
 
-   allocate(center_coordinates(num_centers*3))
+   proton_charges(1) = 8
+   proton_charges(2) = 1
+   proton_charges(3) = 1
 
-   center_coordinates(1) =  0.00d0
-   center_coordinates(2) =  0.00d0
-   center_coordinates(3) =  0.00d0
-   center_coordinates(4) =  1.43d0
-   center_coordinates(5) =  0.00d0
-   center_coordinates(6) =  1.10d0
-   center_coordinates(7) = -1.43d0
-   center_coordinates(8) =  0.00d0
-   center_coordinates(9) =  1.10d0
+   x_coordinates_au(1) = 0.0d0
+   x_coordinates_au(2) = 1.43d0
+   x_coordinates_au(3) = -1.43d0
 
-   allocate(center_elements(num_centers))
+   y_coordinates_au(1) = 0.0d0
+   y_coordinates_au(2) = 0.0d0
+   y_coordinates_au(3) = 0.0d0
 
-   center_elements(1) = 8
-   center_elements(2) = 1
-   center_elements(3) = 1
+   z_coordinates_au(1) = 0.0d0
+   z_coordinates_au(2) = 1.1d0
+   z_coordinates_au(3) = 1.1d0
 
-   num_outer_centers = 0
-
-   num_shells = 12
-
-   allocate(shell_centers(num_shells))
-
-   shell_centers( 1) = 1
-   shell_centers( 2) = 1
-   shell_centers( 3) = 1
-   shell_centers( 4) = 1
-   shell_centers( 5) = 1
-   shell_centers( 6) = 1
-   shell_centers( 7) = 2
-   shell_centers( 8) = 2
-   shell_centers( 9) = 2
-   shell_centers(10) = 3
-   shell_centers(11) = 3
-   shell_centers(12) = 3
-
-   allocate(shell_l_quantum_numbers(num_shells))
-
-   shell_l_quantum_numbers( 1) = 0
-   shell_l_quantum_numbers( 2) = 0
-   shell_l_quantum_numbers( 3) = 0
-   shell_l_quantum_numbers( 4) = 1
-   shell_l_quantum_numbers( 5) = 1
-   shell_l_quantum_numbers( 6) = 2
-   shell_l_quantum_numbers( 7) = 0
-   shell_l_quantum_numbers( 8) = 0
-   shell_l_quantum_numbers( 9) = 1
-   shell_l_quantum_numbers(10) = 0
-   shell_l_quantum_numbers(11) = 0
-   shell_l_quantum_numbers(12) = 1
-
-   allocate(shell_num_primitives(num_shells))
-
-   shell_num_primitives( 1) = 9
-   shell_num_primitives( 2) = 9
-   shell_num_primitives( 3) = 1
-   shell_num_primitives( 4) = 4
-   shell_num_primitives( 5) = 1
-   shell_num_primitives( 6) = 1
-   shell_num_primitives( 7) = 4
-   shell_num_primitives( 8) = 1
-   shell_num_primitives( 9) = 1
-   shell_num_primitives(10) = 4
-   shell_num_primitives(11) = 1
-   shell_num_primitives(12) = 1
-
-   allocate(primitive_exponents(37))
-
-   primitive_exponents( 1) = 1.172d+04
-   primitive_exponents( 2) = 1.759d+03
-   primitive_exponents( 3) = 4.008d+02
-   primitive_exponents( 4) = 1.137d+02
-   primitive_exponents( 5) = 3.703d+01
-   primitive_exponents( 6) = 1.327d+01
-   primitive_exponents( 7) = 5.025d+00
-   primitive_exponents( 8) = 1.013d+00
-   primitive_exponents( 9) = 3.023d-01
-   primitive_exponents(10) = 1.172d+04
-   primitive_exponents(11) = 1.759d+03
-   primitive_exponents(12) = 4.008d+02
-   primitive_exponents(13) = 1.137d+02
-   primitive_exponents(14) = 3.703d+01
-   primitive_exponents(15) = 1.327d+01
-   primitive_exponents(16) = 5.025d+00
-   primitive_exponents(17) = 1.013d+00
-   primitive_exponents(18) = 3.023d-01
-   primitive_exponents(19) = 3.023d-01
-   primitive_exponents(20) = 1.770d+01
-   primitive_exponents(21) = 3.854d+00
-   primitive_exponents(22) = 1.046d+00
-   primitive_exponents(23) = 2.753d-01
-   primitive_exponents(24) = 2.753d-01
-   primitive_exponents(25) = 1.185d+00
-   primitive_exponents(26) = 1.301d+01
-   primitive_exponents(27) = 1.962d+00
-   primitive_exponents(28) = 4.446d-01
-   primitive_exponents(29) = 1.220d-01
-   primitive_exponents(30) = 1.220d-01
-   primitive_exponents(31) = 7.270d-01
-   primitive_exponents(32) = 1.301d+01
-   primitive_exponents(33) = 1.962d+00
-   primitive_exponents(34) = 4.446d-01
-   primitive_exponents(35) = 1.220d-01
-   primitive_exponents(36) = 1.220d-01
-   primitive_exponents(37) = 7.270d-01
-
-   context = numgrid_new_context()
-
-   call numgrid_generate_grid(context,                  &
-                              radial_precision,         &
-                              min_num_angular_points,   &
-                              max_num_angular_points,   &
-                              num_centers,              &
-                              center_coordinates,       &
-                              center_elements,          &
-                              num_outer_centers,        &
-                              outer_center_coordinates, &
-                              outer_center_elements,    &
-                              num_shells,               &
-                              shell_centers,            &
-                              shell_l_quantum_numbers,  &
-                              shell_num_primitives,     &
-                              primitive_exponents)
-
-   num_points = numgrid_get_num_points(context)
-
-   if (num_points /= 46220) stop 1
-
-   grid => numgrid_get_grid(context)
+   ! oxygen
+   alpha_max = 11720.0d0
+   max_l_quantum_number = 2
+   alpha_min(1) = 0.3023d0
+   alpha_min(2) = 0.2753d0
+   alpha_min(3) = 1.185d0
 
    open(unit=io_unit, file='../test/reference_grid.txt', access='sequential', action='read')
-   k = 1
-   do i = 1, num_points
+
+   context = numgrid_new_atom_grid(radial_precision,       &
+                                   min_num_angular_points, &
+                                   max_num_angular_points, &
+                                   proton_charges(1),      &
+                                   alpha_max,              &
+                                   max_l_quantum_number,   &
+                                   alpha_min)
+
+   num_points = numgrid_get_num_grid_points(context)
+   if (num_points /= 16364) stop 1
+
+   allocate(grid_x_au(num_points))
+   allocate(grid_y_au(num_points))
+   allocate(grid_z_au(num_points))
+   allocate(grid_w(num_points))
+
+   call numgrid_get_grid_points(context,          &
+                                num_centers,      &
+                                center_index,     &
+                                x_coordinates_au, &
+                                y_coordinates_au, &
+                                z_coordinates_au, &
+                                proton_charges,   &
+                                grid_x_au,        &
+                                grid_y_au,        &
+                                grid_z_au,        &
+                                grid_w)
+
+   do ipoint = 1, num_points
       read(io_unit, *) ref(1), ref(2), ref(3), ref(4)
+      own(1) = grid_x_au(ipoint)
+      own(2) = grid_y_au(ipoint)
+      own(3) = grid_z_au(ipoint)
+      own(4) = grid_w(ipoint)
       do j = 1, 4
-         error = grid(k) - ref(j)
+         error = own(j) - ref(j)
          if (dabs(ref(j)) > 1.0e-15) error = error/ref(j)
          if (dabs(error) > 1.0e-5) stop 1
-         k = k + 1
       end do
    end do
    close(io_unit)
 
-   deallocate(center_coordinates)
-   deallocate(center_elements)
-   deallocate(shell_centers)
-   deallocate(shell_l_quantum_numbers)
-   deallocate(shell_num_primitives)
-   deallocate(primitive_exponents)
+   deallocate(grid_x_au)
+   deallocate(grid_y_au)
+   deallocate(grid_z_au)
+   deallocate(grid_w)
 
-   call numgrid_free_context(context)
+   call numgrid_free_atom_grid(context)
 
 end program
