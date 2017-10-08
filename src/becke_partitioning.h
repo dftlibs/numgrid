057bae0c9fdc361581f6ed013c77259eba95ea3b
Radovan Bast <bast@users.noreply.github.com>
2017-10-08 13:12:53 +0200

    rm leftover code

--- src/becke_partitioning.h
+++ src/becke_partitioning.h
@@ -1,14 +1,6 @@
 #ifndef BECKE_PARTITIONING_H_INCLUDED
 #define BECKE_PARTITIONING_H_INCLUDED
 
-double get_becke_w2(const double center_coordinates[],
-                    const int center_charge[],
-                    double pa[],
-                    const int icent,
-                    const int num_centers,
-                    const double x,
-                    const double y,
-                    const double z);
 double get_becke_w(const int num_centers,
                    const int proton_charges[],
                    const double x_coordinates_au[],
