c1fa1a64d9a902e8010b0ac6bde99dd8c03802bd
Radovan Bast <bast@users.noreply.github.com>
2017-10-09 10:34:21 +0200

    refactor

--- cmake/custom/include_paths.cmake
+++
@@ -1,5 +0,0 @@
-include_directories(${PROJECT_SOURCE_DIR}/include)
-add_subdirectory(${PROJECT_SOURCE_DIR}/include)
-
-file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/include)
-include_directories(${PROJECT_BINARY_DIR}/include)
